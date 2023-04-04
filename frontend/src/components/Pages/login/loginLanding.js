32df2231e29aebd398a3a0f8419108ba2ca182ad
Jahedul Islam <jahedulmd1232@gmail.com>
2023-04-04 17:16:14 +0600

    test name and result

--- frontend/src/components/Pages/login/loginLanding.js
+++ frontend/src/components/Pages/login/loginLanding.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , useRef  } from "react";
 import { ethers } from "ethers";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { useLocation } from "react-router-dom";
@@ -49,41 +49,81 @@ const LoginLandingPage = () => {
   const [add, setAddress] = useState("");
   const [height, setHeight] = useState("");
   const [token, setToken] = useState("");
+  const [report, setReport] = useState("");
+  // const [test1, setTest1] = useState();
+  // const [result1, setResult1] = useState();
+  // const [test2, setTest2] = useState();
+  // const [result2, setResult2] = useState();
+  // const [test3, setTest3] = useState();
+  // const [result3, setResult3] = useState();
   const navigate = useNavigate();
-  var [dataArray, setDataArray] = useState([]);
-  // const [genete, setGenerate] = useState("");
-  // const [user, setUser] = useState();
+  // var [dataArray, setDataArray] = useState([]);
+  let [testArray, setTestArray] = useState([]);
+  let [resultArray, setResultArray] = useState([]);
+  var [testName,setTestName] = useState();
+  var [resultName,setResultName] = useState();
+  var crypto = require("crypto-js");
+  let hasRunOnce = useRef(false);
 
   const { ethereum } = window;
   const location = useLocation();
   const docRef = doc(db, "records", location.state.userID);
   let contract;
+  let val_temp = 0;
 
   const fetchData = async (recordsDocs) => {
     // const user = await auth.currentUser.uid;
-    if (recordsDocs.exists) {
+    console.log("inside fetchData", recordsDocs[0]);
+    val_temp = 1;
+    console.log("val_temp is ",val_temp)
+    if (recordsDocs.length > 0) {
       console.log("inside if condition.....");
       console.log("docs are : ", recordsDocs);
       let dataDocs = recordsDocs[0].record;
       // console.log("specific ciphertext is : ", dataDocs);
-      var crypto = require("crypto-js");
+      
       // const secretKey = "my-secret-key@123";
-      var keySecret = "my-secret-key@123";
+      let keySecret = "my-secret-key@123";
 
       // Decrypt
-      var recordsBytes = crypto.AES.decrypt(dataDocs, keySecret);
-      var decryptedRecords = await JSON.parse(
+      let recordsBytes = crypto.AES.decrypt(dataDocs, keySecret);
+      let decryptedRecords = await JSON.parse(
         recordsBytes.toString(crypto.enc.Utf8)
       );
-      console.log("decrypted data is : ", decryptedRecords);
+      console.log("decrypted data is : ", decryptedRecords[0]);
+      // setDataArray(decryptedRecords);
+      console.log("first values are : ", decryptedRecords[0].test1, decryptedRecords[0].result1);
+      var first_test = await decryptedRecords[0].test1;
+      var second_test = await decryptedRecords[0].test2;
+      var third_test = await decryptedRecords[0].test3;
+      var first_result =await decryptedRecords[0].result1;
+      var second_result =await decryptedRecords[0].result2;
+      var third_result =await decryptedRecords[0].result3;
+      console.log("first_test is : ", first_test);
+      setTestArray((testArray)=>[...testArray, first_test]);
+      setResultArray((resultArray)=>[...resultArray, first_result]);
+      setTestArray((testArray)=>[...testArray, second_test]);
+      setResultArray((resultArray)=>[...resultArray, second_result]);
+      setTestArray((testArray)=>[...testArray, third_test]);
+      setResultArray((resultArray) => [...resultArray, third_result]);
+      setResultName("Result")
+      setTestName("Test Name")
+
+      hasRunOnce.current = true;
     } else {
       console.log("there is no record");
     }
+
+    console.log("Array size : ", testArray);
   };
   const user = auth.currentUser.uid;
   const query = collection(db, "records", user, "Medical-record");
   const [recordsDocs, loading, error] = useCollectionData(query);
-  fetchData(recordsDocs);
+  if (!hasRunOnce.current) {
+    fetchData(recordsDocs);
+    console.log("inside fethch call...")
+  }
+  
 
   useEffect(() => {
     function makeid(length) {
@@ -100,6 +140,15 @@ const LoginLandingPage = () => {
       return result;
     }
     setToken(makeid(6));
+
+
+    function getData(data) {
+      console.log("inside useEffect ",data)
+    }
+
+
+
+
   }, []);
 
   const connectMetamask = async () => {
@@ -176,6 +225,8 @@ const LoginLandingPage = () => {
     setHeight(decryptedData[0].gender);
     // alert("Data is decrypted Successfully.....");
     console.log("read successfully");
+
+    
   };
 
   const Read = async (params) => {
@@ -268,14 +319,13 @@ const LoginLandingPage = () => {
               <button class="btn btn-primary" onClick={ReadDatafromHIS}>
                 Read Data
               </button>
-              <div>
-                {dataArray.map((item) => (
-                  <div key={item.id}>
-                    <h2>{item.title}</h2>
-                    <p>{item.description}</p>
+              {/* <div>
+                {testArray.map((item,index) => (
+                  <div >
+                     <li key={index}>{item}</li>
                   </div>
                 ))}
-              </div>
+              </div> */}
 
               {/* <h3>Anyone can Rea</h3> */}
             </div>
@@ -289,17 +339,140 @@ const LoginLandingPage = () => {
           />
         </div>
       </div>
-      <div className="container">
-        <div class="card">
+      <div>
+        
+{/* <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Color
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Category
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Price
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    Laptop
+                </td>
+                <td class="px-6 py-4">
+                    $2999
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+                <td class="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td class="px-6 py-4">
+                    $1999
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Google Pixel Phone
+                </th>
+                <td class="px-6 py-4">
+                    Gray
+                </td>
+                <td class="px-6 py-4">
+                    Phone
+                </td>
+                <td class="px-6 py-4">
+                    $799
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple Watch 5
+                </th>
+                <td class="px-6 py-4">
+                    Red
+                </td>
+                <td class="px-6 py-4">
+                    Wearables
+                </td>
+                <td class="px-6 py-4">
+                    $999
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div> */}
+
+      </div>
+      <div className="container mt-5">
+        <div class="card shadow mb-5">
           <div class="card-body">
+            {/* <div>{report}</div> */}
             {loading && "Loading.."}
-            <ul>
-              {recordsDocs?.map((doc) => (
-                <div key={Math.random()}>
-                  <li>{doc.record}</li>
-                </div>
-              ))}
-            </ul>
+            <table class="w-full text-lg text-left text-gray-500 dark:text-gray-400" > {/*style={{ 'max-width': '300px' }} */}
+              <thead class="text-m text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <th scope="col" class="px-6 py-3">{testName}</th>
+                <th scope="col" class="px-6 py-3">{resultName}</th>
+              </thead>
+              <tbody>
+                {testArray.map((item,index) => (
+                  <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th key={index} scope="row" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {item}</th>
+                    <td>{resultArray[index] }</td>
+                  </tr>
+                ))}
+                </tbody>
+            </table>
           </div>
         </div>
       </div>
